c502c3975bf944160a754dbaa3ed8726e2487f60
sahoo727 <soubhagyasahoo727@gmail.com>
2024-11-28 10:08:23 +0530

    chore: production ready

--- server/index.js
+++ server/index.js
@@ -33,8 +33,8 @@ app.use((err, req, res, next) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running ${PORT}`);
-  });
+  console.log(`Server is running ${PORT}`);
+});
